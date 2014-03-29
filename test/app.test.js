8527adcdd1404cae384bbccddc777fa7413980ed
Mike Jones <mike@westerncapelabs.com>
2014-03-29 16:05:44 +0200

    Add tests for invalid and unmapped API calls

--- test/app.test.js
+++ test/app.test.js
@@ -26,35 +26,104 @@ describe("app", function() {
         });
 
         describe("when they have not been pre-registered",function() {
-            it("should acknowledge and register them", function() {
-                return tester
-                    .setup.user.addr('+27123')
-                    .start()
-                    .input({
-                        content:'141002',
-                        to_addr: '10010',
-                        transport_metadata: {
-                            'netcore': {
-                                'circle': "of life",
-                                'source': "sms",
+            describe("entering a valid PIN", function() {
+                it("should acknowledge and register them", function() {
+                    return tester
+                        .setup.user.addr('+27123')
+                        .start()
+                        .input({
+                            content:'141002',
+                            to_addr: '10010',
+                            transport_metadata: {
+                                'netcore': {
+                                    'circle': "of life",
+                                    'source': "sms",
+                                }
                             }
-                        }
-                    })
-                    .check.interaction({
-                        state: 'states:start',
-                        reply: ["MYNETA:candidates details.You can visit our website ", 
-                                "www.myneta.info for full details of candidates or call ",
-                                "toll free 1800-110-440 to get them on phone"]
-                                .join('')
-                    })
-                    .check(function(api) {
-                        var contact = api.contacts.store[0];
-                        assert.equal(contact.extra.circle, 'of life');
-                        assert.equal(contact.extra.registration_source, 'sms');
-                        assert.equal(contact.extra.source_addr, '10010');
-                        assert.equal(contact.extra.pin, '141002');
-                    })
-                    .run();
+                        })
+                        .check.interaction({
+                            state: 'states:start',
+                            reply: ["MYNETA:candidates details.You can visit our website ", 
+                                    "www.myneta.info for full details of candidates or call ",
+                                    "toll free 1800-110-440 to get them on phone"]
+                                    .join('')
+                        })
+                        .check(function(api) {
+                            var contact = api.contacts.store[0];
+                            assert.equal(contact.extra.circle, 'of life');
+                            assert.equal(contact.extra.registration_source, 'sms');
+                            assert.equal(contact.extra.source_addr, '10010');
+                            assert.equal(contact.extra.pin, '141002');
+                        })
+                        .run();
+                });
+            });
+            describe("entering an invalid PIN", function() {
+                it("should acknowledge error and register them", function() {
+                    return tester
+                        .setup.user.addr('+27123')
+                        .start()
+                        .input({
+                            content:'14100',
+                            to_addr: '10010',
+                            transport_metadata: {
+                                'netcore': {
+                                    'circle': "of life",
+                                    'source': "sms",
+                                }
+                            }
+                        })
+                        .check.interaction({
+                            state: 'states:start',
+                            reply: ["MYNETA: Please send a valid six digit pincode or ", 
+                                    "constituency name.Visit www.myneta.info or call ",
+                                    "1800-110-440 to get more details of candidates"]
+                                    .join('')
+                        })
+                        .check(function(api) {
+                            var contact = api.contacts.store[0];
+                            assert.equal(contact.extra.circle, 'of life');
+                            assert.equal(contact.extra.registration_source, 'sms');
+                            assert.equal(contact.extra.source_addr, '10010');
+                            assert.equal(contact.extra.pin, '14100');
+                        })
+                        .run();
+                });
+            });
+            describe("entering an unmapped PIN", function() {
+                it("should acknowledge issue and register them", function() {
+                    return tester
+                        .setup.user.addr('+27123')
+                        .setup.char_limit(300)
+                        .start()
+                        .input({
+                            content:'149099',
+                            to_addr: '10010',
+                            transport_metadata: {
+                                'netcore': {
+                                    'circle': "of life",
+                                    'source': "sms",
+                                }
+                            }
+                        })
+                        .check.interaction({
+                            state: 'states:start',
+                            reply: ["MYNETA: Despite our best efforts, pincode 149099 is ",
+                                    "still not mapped to its right constituency in our database ",
+                                    "yet. We are working on that, in the mean time you can send ",
+                                    "constituency name instead of pincode.Visit www.myneta.info ",
+                                    "or call 1800-110-440 to get more details of candidates"]
+                                    .join('')
+                        })
+                        .check(function(api) {
+                            var contact = api.contacts.store[0];
+                            assert.equal(contact.extra.circle, 'of life');
+                            assert.equal(contact.extra.registration_source, 'sms');
+                            assert.equal(contact.extra.source_addr, '10010');
+                            assert.equal(contact.extra.pin, '149099');
+                        })
+                        .run();
+                });
             });
         });
 
